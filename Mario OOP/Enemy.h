890252e96b2f394e81fc132690f26303a035c3d3
Basil2004 <66377008+Basil2004@users.noreply.github.com>
2024-12-03 17:19:30 +0500

    Update Enemy.h

--- Mario OOP/Enemy.h
+++ Mario OOP/Enemy.h
@@ -4,20 +4,20 @@
 #include <SFML/Graphics.hpp>
 #include "Character.h"
 
-class Enemy : public Character {
+class Enemy:public Character {
 public:
-    Enemy(float startX, float startY);  // Constructor
-    void update(float marioX, float screenLeft, float screenRight);  // Update position and behavior
-    void draw(sf::RenderWindow& window) const;  // Render the enemy
-    void die();  // Mark enemy as dead
-    bool isDead() const;  // Check if the enemy is dead
-    sf::FloatRect getBounds() const;  // Get enemy bounds for collision detection
+    Enemy(float startX, float startY);  // constructor
+    void update(float marioX, float screenLeft, float screenRight);  // update position and behavior
+    void draw(sf::RenderWindow& window) const;  // render the enemy
+    void die();  // marrk enemy as dead
+    bool isDead() const;  // check if the enemy is dead
+    sf::FloatRect getBounds() const;  // get enemy bounds for collision detection
 
 private:
-    sf::Texture aliveTexture;  // Texture when the enemy is alive
-    sf::Texture deadTexture;   // Texture when the enemy is dead
-    float speed;               // Movement speed
-    bool dead;                 // Status of the enemy (alive or dead)
+    sf::Texture aliveTexture;  // texture when the enemy is alive
+    sf::Texture deadTexture;   // texture when the enemy is dead
+    float speed;               // movement speed
+    bool dead;                 // status of the enemy (alive or dead)
 };
 
 #endif
